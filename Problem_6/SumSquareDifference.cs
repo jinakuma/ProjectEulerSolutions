6f1058130edbbb790d9ac5eb9a4b44681297f0fb
Murat Utlu <utlumurat@gmail.com>
2021-08-10 11:24:25 +0300

    ProjectEuler Problem 6 added

---
+++ Problem_6/SumSquareDifference.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_6
+{
+    //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    public static class SumSquareDifference
+    {
+        public static void FirstHundredNumbersSumSquareDifference()
+        {
+            int squareSum = 0;
+            int sumSquare = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                squareSum += i*i;
+            }
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sumSquare += i;
+            }
+
+            sumSquare *= sumSquare;
+            Console.WriteLine(sumSquare-squareSum);
+        }
+    }
+}
--- ProjectEulerSolutions/Program.cs
+++ ProjectEulerSolutions/Program.cs
@@ -4,6 +4,7 @@ using Problem_2;
 using Problem_3;
 using Problem_4;
 using Problem_5;
+using Problem_6;
 
 namespace ProjectEulerSolutions
 {
@@ -11,7 +12,7 @@ namespace ProjectEulerSolutions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(SmallestMultiple.SmallestMultipleOfTwenty());
+            SumSquareDifference.FirstHundredNumbersSumSquareDifference();
             Console.ReadLine();
         }
     }
