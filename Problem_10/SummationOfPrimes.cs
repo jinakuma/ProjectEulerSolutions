eeca6415443fff40c1a68bb1b0fc66da55e8a63d
Murat Utlu <utlumurat@gmail.com>
2021-08-14 22:09:13 +0300

    Project Euler Problem 10 Solution Added.

---
+++ Problem_10/SummationOfPrimes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_10
+{
+    public static class SummationOfPrimes
+    {
+        public static long GeneratePrimesNaive(int n)
+        {
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            int nextPrime = 3;
+            while (primes[^1] < n)
+            {
+                int sqrt = (int)Math.Sqrt(nextPrime);
+                bool isPrime = true;
+                for (int i = 0; (int)primes[i] <= sqrt; i++)
+                {
+                    if (nextPrime % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes.Add(nextPrime);
+                }
+                nextPrime += 2;
+            }
+
+            long sum =0;
+            foreach (var prime in primes)
+            {
+                //Console.WriteLine(prime);
+                sum += Convert.ToInt64(prime);
+            }
+
+            sum -= primes[^1];
+
+            return sum;
+        }
+    }
+}
--- ProjectEulerSolutions/Program.cs
+++ ProjectEulerSolutions/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using Problem_9;
-
+using Problem_10;
 namespace ProjectEulerSolutions
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(SpecialPythagoreanTriplet.ProductOfPythagoreanTriplet(1000));
-            ;
+            Console.WriteLine(SummationOfPrimes.GeneratePrimesNaive(2000000));
             Console.ReadLine();
         }
     }
